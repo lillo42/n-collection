ff4f197207be59735b9b11aab32d9a725afcf9c4
Rafael Andrade <rafael.andrade@sky.uk>
2020-09-04 14:00:52 +0100

    Improve IMap and AbstractMap.cs

---
+++ src/NCollection/AbstractMap.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace NCollection
+{
+    /// <summary>
+    /// This class provides a skeletal implementation of the <see cref="IMap{TKey,TValue}"/>
+    /// interface, to minimize the effort required to implement this interface.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    [DebuggerTypeProxy(typeof(ICollectionDebugView<>))]
+    [DebuggerDisplay("Count = {Count}")]
+    public abstract class AbstractMap<TKey, TValue> : AbstractCollection<KeyValuePair<TKey, TValue>>,  IMap<TKey, TValue>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IEqualityComparer<TKey> Comparer { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected AbstractMap()
+        {
+            
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="comparer"></param>
+        protected AbstractMap([NotNull] IEqualityComparer<TKey> comparer)
+        { 
+            Comparer = comparer ?? throw new ArgumentNullException(nameof(comparer));
+
+        }
+        
+        /// <inheritdoc cref="System.Collections.Generic.IDictionary{TKey, TValue}"/>
+        public abstract TValue this[TKey key] { get; set; }
+        
+        /// <inheritdoc cref="IMap{TKey, TValue}"/>
+        public abstract ISet<IMap<TKey, TValue>.IEntry> Entries { get; }
+
+        /// <inheritdoc cref="IMap{TKey, TValue}"/>
+        public abstract ISet<TKey> Keys { get; }
+        
+        /// <inheritdoc cref="IMap{TKey, TValue}"/>
+        public abstract ICollection<TValue> Values { get; }
+        
+        /// <inheritdoc cref="System.Collections.Generic.IDictionary{TKey, TValue}"/>
+        public virtual void Add(TKey key, TValue value)
+        {
+            if (!TryAdd(new KeyValuePair<TKey, TValue>(key, value)))
+            {
+                throw new InvalidOperationException("Duplicated key");
+            }
+        }
+        
+        /// <inheritdoc cref="IMap{TKey, TValue}"/>
+        public abstract bool TryAdd(TKey key, TValue value);
+
+        /// <inheritdoc />
+        public override bool TryAdd(KeyValuePair<TKey, TValue> item)
+        {
+            return TryAdd(item.Key, item.Value);
+        }
+
+        /// <inheritdoc cref="System.Collections.Generic.IDictionary{TKey, TValue}"/>
+        public abstract bool Remove(TKey key);
+
+        /// <inheritdoc />
+        public override bool Remove(KeyValuePair<TKey, TValue> item) => Remove(item.Key);
+
+        /// <inheritdoc cref="System.Collections.Generic.IDictionary{TKey, TValue}"/>
+        public abstract bool TryGetValue(TKey key, out TValue value);
+
+        /// <inheritdoc cref="System.Collections.Generic.IDictionary{TKey, TValue}"/>
+        public virtual bool ContainsKey(TKey key)
+        {
+            foreach (var item in Keys)
+            {
+                if (Comparer.Equals(item, key))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc cref="IMap{TKey, TValue}"/>
+        public virtual bool ContainsValue(TValue value)
+        {
+            foreach (var item in Values)
+            {
+                if (EqualityComparer<TValue>.Default.Equals(item, value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- src/NCollection/IMap.cs
+++ src/NCollection/IMap.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NCollection
 {
@@ -10,6 +11,11 @@ namespace NCollection
     /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
     public interface IMap<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>>, IDictionary<TKey, TValue>
     {
+        ISet<IEntry> Entries { get; }
+        
+        new ISet<TKey> Keys { get; }
+        new ICollection<TValue> Values { get; }
+        
         /// <summary>
         /// Returns true if this map maps one or more keys to the
         /// specified value.  More formally, returns true if and only if
@@ -22,6 +28,15 @@ namespace NCollection
         /// <returns></returns>
         bool ContainsValue(TValue value);
 
+        
+        /// <summary>
+        ///  Try adds an item to the <see cref="IMap{TKey,TValue}"/>
+        /// </summary>
+        /// <param name="key">The <typeparamref name="TKey"/> </param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool TryAdd(TKey key, TValue value);
+        
         /// <summary>
         /// Copies all of the mappings from the specified map to this map
         /// (optional operation).  The effect of this call is equivalent to that
@@ -31,36 +46,43 @@ namespace NCollection
         /// specified map is modified while the operation is in progress.
         /// </summary>
         /// <param name="source"></param>
-        void AddRange(IMap<TKey, TValue> source)
+        void AddAll(IMap<TKey, TValue> source)
         {
             foreach (var (key, value) in source)
             {
                 Add(key, value);
             }
         }
+
+        #region IDictionary
+
+        System.Collections.Generic.ICollection<TKey> IDictionary<TKey, TValue>.Keys => Keys;
+        
+        System.Collections.Generic.ICollection<TValue> IDictionary<TKey, TValue>.Values => Values;
+
+        #endregion
         
         /// <summary>
-        /// If the specified key is not already associated with a value (or is mapped
-        /// to null) associates it with the given value and returns
-        /// null, else returns the current value.
+        /// A map entry (key-value pair).  The {@code Map.entrySet} method returns
+        /// a collection-view of the map, whose elements are of this class.  The
+        /// only way to obtain a reference to a map entry is from the
+        /// iterator of this collection-view.  These {@code Map.Entry} objects are
+        /// valid only for the duration of the iteration; more formally,
+        /// the behavior of a map entry is undefined if the backing map has been
+        /// modified after the entry was returned by the iterator, except through
+        /// the {@code setValue} operation on the map entry.
         /// </summary>
-        /// <param name="key">key with which the specified value is to be associated</param>
-        /// <param name="value">value to be associated with the specified ke</param>
-        /// <returns> The previous value associated with the specified key, or
-        /// null if there was no mapping for the key.
-        /// (A null return can also indicate that the map
-        /// previously associated null with the key,
-        /// if the implementation supports null values.)
-        /// </returns>
-        TValue AddIfAbsent(TKey key, TValue value)
+        public interface IEntry
         {
-            if (!ContainsKey(key))
-            {
-                Add(key, value);
-                return value;
-            }
-
-            return this[key];
+            /// <summary>
+            /// The key corresponding to this entry
+            /// </summary>
+            TKey Key { get; }
+            
+            /// <summary>
+            /// The value corresponding to this entry
+            /// </summary>
+            TValue Value { get; set; }
         }
     }
 }
